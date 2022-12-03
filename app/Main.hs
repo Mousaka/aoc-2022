1f4dd071ec330f3dbfac6bb229f771edc838a562
Kristian Lundström <Mousaka@users.noreply.github.com>
2022-12-03 12:41:02 +0100

    first

---
+++ app/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Day1.MaxCalories
+import Day2.RockPaper
+
+-- main :: IO ()
+-- main =
+--   mapM_ print [Day1.MaxCalories.solverA, Day1.MaxCalories.solverB, Day2.RockPaper.solverA, Day2.RockPaper.solverB]
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
